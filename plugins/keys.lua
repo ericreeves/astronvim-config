016751aafd6695ecbf9882ee4078409a205f726f
Eric Reeves <eric@alluvium.com>
2023-06-20 03:46:36 +0000

    More plugins

---
+++ plugins/keys.lua
@@ -0,0 +1,4 @@
+lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.keys.normal_mode["<leader>|"] = ":vsplit<CR>"
+lvim.keys.normal_mode["<leader>-"] = ":split<CR>"
+-- aligh cursor on screen center during scroll
---
+++ top-tpipeline-bar.lua
@@ -0,0 +1,4 @@
+-- https://github.com/vimpostor/vim-tpipeline/pull/22
+let g:tpipeline_split = 1
+let g:tpipeline_usepane = 1
+let g:tpipeline_fillcentre = 1
